5a22565afcbed47aad5482c7b18c3fa56e64484c
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-08-22 11:13:16 +0900

    commit 6

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductCard = ({}) => {
+const ProductCard = () => {
   return (
     <div>
       <img src="https://lp2.hm.com/hmgoepprod?set=source[/97/5b/975b7792b82ac8fa775bad09e948f22683c5e5a7.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]"
